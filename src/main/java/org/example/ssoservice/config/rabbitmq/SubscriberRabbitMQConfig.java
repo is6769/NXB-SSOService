3944dfadba089e8db94d9077452442a133e5ea99
is6769 <igor.shvets2007@mail.ru>
2025-05-08 12:56:10 +0300

    added dlq and made user creation response returning newSubscriber

--- src/main/java/org/example/ssoservice/config/rabbitmq/SubscriberRabbitMQConfig.java
+++ src/main/java/org/example/ssoservice/config/rabbitmq/SubscriberRabbitMQConfig.java
@@ -17,6 +17,35 @@ public class SubscriberRabbitMQConfig {
     @Value("${const.rabbitmq.subscriber.SUBSCRIBER_CREATED_ROUTING_KEY}")
     private String SUBSCRIBER_CREATED_ROUTING_KEY;
 
+
+    @Value("${const.rabbitmq.dead-letter.DEAD_LETTER_EXCHANGE_POSTFIX}")
+    private String DEAD_LETTER_EXCHANGE_POSTFIX;
+
+    @Value("${const.rabbitmq.dead-letter.DEAD_LETTER_ROUTING_KEY_POSTFIX}")
+    private String DEAD_LETTER_ROUTING_KEY_POSTFIX;
+
+    @Value("${const.rabbitmq.dead-letter.DEAD_LETTER_QUEUE_POSTFIX}")
+    private String DEAD_LETTER_QUEUE_POSTFIX;
+
+
+    @Bean
+    public DirectExchange deadLetterSubscriberCreatedExchange(){
+        return new DirectExchange(SUBSCRIBER_CREATED_EXCHANGE_NAME+DEAD_LETTER_EXCHANGE_POSTFIX,false,false);
+    }
+
+    @Bean
+    public Queue deadLetterSubscriberCreatedQueue(){
+        return new Queue(SUBSCRIBER_CREATED_QUEUE_NAME+DEAD_LETTER_QUEUE_POSTFIX);
+    }
+
+    @Bean
+    public Binding deadLetterSubscriberCreatedBinding(){
+        return BindingBuilder
+                .bind(deadLetterSubscriberCreatedQueue())
+                .to(deadLetterSubscriberCreatedExchange())
+                .with(SUBSCRIBER_CREATED_ROUTING_KEY+DEAD_LETTER_ROUTING_KEY_POSTFIX);
+    }
+
     @Bean
     public DirectExchange subscriberCreatedExchange(){
         return new DirectExchange(SUBSCRIBER_CREATED_EXCHANGE_NAME,false,false);
